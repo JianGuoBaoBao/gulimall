8865e5c796bcf36e2ee98478cbdd1fecfff9d4ec
坚果宝宝 <791551390@qq.com>
2023-08-29 23:06:38 +0800

    [firstCommit] init project P18 [jianguobaobao]

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/dao/SpuInfoDescDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.product.dao;
+
+import com.atguigu.gulimall.product.entity.SpuInfoDescEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * spu信息介绍
+ * 
+ * @author jianguobaobao
+ * @email jianguobaobao@gmail.com
+ * @date 2023-08-29 22:25:56
+ */
+@Mapper
+public interface SpuInfoDescDao extends BaseMapper<SpuInfoDescEntity> {
+	
+}
