7f7c29fd5f366ac4e8a029f25dda9a817ebfee68
坚果宝宝 <791551390@qq.com>
2023-10-07 01:08:54 +0800

    [firstCommit] gulimall 商品分类调整 P78 Api平台属性-规格修改 [jianguobaobao]

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/AttrEntity.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/AttrEntity.java
@@ -1,5 +1,6 @@
 package com.atguigu.gulimall.product.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
@@ -9,7 +10,7 @@ import lombok.Data;
 
 /**
  * 商品属性
- * 
+ *
  * @author jianguobaobao
  * @email jianguobaobao@gmail.com
  * @date 2023-08-29 22:25:57
