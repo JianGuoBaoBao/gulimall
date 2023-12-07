6be43956df50a0320152a97de2118730b35e96fc
wanglingjie <wanglingjie@xxkjjt.wecom.work>
2023-12-07 17:30:09 +0800

    [firstCommit] gulimall 商品分类调整 P127 10:00 全文检索-ElasticSearch整合 [jianguobaobao]

--- gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
@@ -1,6 +1,14 @@
 package com.atguigu.gulimall.search;
 
+import com.alibaba.fastjson.JSON;
+import com.atguigu.gulimall.search.config.GuilimallElasticSearchConfig;
+import javafx.scene.control.IndexRange;
+import lombok.Data;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.rest.action.cat.RestHealthAction;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,11 +16,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
 import java.sql.SQLOutput;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallSearchApplicationTests {
+public class GulimallSearchApplicationTests {
 
     @Autowired
     private RestHighLevelClient client;
@@ -21,8 +30,30 @@ class GulimallSearchApplicationTests {
      * 测试存储数据到es
      */
     @Test
-    public void indexData() {
-        
+    public void indexData() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("users");
+        indexRequest.id("1");
+//        indexRequest.source("userName","zhangsan","age",18,"gender","男");
+
+        User user = new User();
+        user.setUserName("zhangsan");
+        user.setAge(18);
+
+        String jsonString = JSON.toJSONString(user);
+        indexRequest.source(jsonString, XContentType.JSON); // 要保存的内容
+
+
+        // 执行操作
+        IndexResponse index = client.index(indexRequest, GuilimallElasticSearchConfig.COMMON_OPTIONS);
+
+        // 提取有用的响应数据
+
+    }
+    @Data
+    class User{
+        private String userName;
+        private String gender;
+        private Integer age;
     }
 
     @Test
